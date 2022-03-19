6c728ad5e0b2fe3140b6c5b52d96e90d132919bd
Xdelta107 <lucapani741@gmail.com>
2022-03-19 18:46:57 +0100

    Fixed FOG in DarkHallWay

--- levels/castle_inside/area_2/macro.inc.c
+++ levels/castle_inside/area_2/macro.inc.c
@@ -3,4 +3,3 @@ const MacroObject castle_inside_area_2_macro_objs[] = {
 	MACRO_OBJECT_WITH_BEH_PARAM(/*preset*/ macro_wooden_signpost,  /*yaw*/ 0, /*pos*/ -943,    -247, -3442, /*behParam*/ DIALOG_016), //wodden sign
 	MACRO_OBJECT_END(),
 };
-
